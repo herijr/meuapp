e87e9a7883dfcc551d0f34dc7f515a03823f65b7
Herivelto Junior <herijr@gmail.com>
2023-03-06 23:18:18 -0300

    versao 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meu App React
+          Meu App React v1
         </p>
       </header>
     </div>
